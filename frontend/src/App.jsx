d4be19efcf15829f64667afd03f4705f2cf49c44
Godfred Obeng <ochucu24681@gmail.com>
2025-07-04 18:32:10 +0100

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import SignalForm from './components/SignalForm';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white text-gray-800">
+      <SignalForm />
+    </div>
+  );
+}
+
+export default App;
