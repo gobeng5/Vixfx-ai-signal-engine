2c439b76a4a61c24e5ac7de050fe3b42430a538a
Godfred Obeng <ochucu24681@gmail.com>
2025-07-04 19:08:03 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
+import React from 'react';
 import SignalForm from './components/SignalForm';
 
-function App() {
+export default function App() {
   return (
     <div className="min-h-screen bg-white text-gray-800">
       <SignalForm />
     </div>
   );
 }
-
-export default App;
