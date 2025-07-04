531596cef254ef367e94bd44845cb1a48912287b
Godfred Obeng <ochucu24681@gmail.com>
2025-07-05 00:47:15 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 export default defineConfig({
   root: 'frontend',
@@ -9,9 +7,6 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: resolve(__dirname, 'frontend/src/index.html') // 👈 tell Vite where to find index.html
-    }
+    emptyOutDir: true
   }
 });
