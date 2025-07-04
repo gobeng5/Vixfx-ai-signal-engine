0f914b247e58fa46e72568fae6aecda743867bab
Godfred Obeng <ochucu24681@gmail.com>
2025-07-05 00:02:21 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,23 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   root: 'frontend',
-  base: '/', // ✅ this is critical for Vercel or any root-hosted app
-
-  plugins: [
-    react({
-      jsxRuntime: 'classic',
-      babel: {
-        plugins: ['react-require']
-      }
-    })
-  ],
-
-  esbuild: {
-    jsxInject: `import React from 'react'`
-  },
-
+  base: '/',
+  plugins: [react()],
   build: {
-    outDir: 'frontend/dist',
+    outDir: 'dist',
     emptyOutDir: true
   }
 });
